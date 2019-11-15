2d7bfa9e89aae2e696120380a73717619821b8c8
Roman Kuznetcov <reiqen@yahoo.com>
2019-11-15 21:35:13 +0400

    Make first release

---
+++ Task0/Corrector.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace com.GitHub.Reiqen
+{
+    /*
+        This class provides methods to check if the number which is read from input is valid for further operations.
+        It has an interface to check if the input value is a number and it is positive and both positive and odd.
+    */
+    class Corrector
+    {
+        // Checks if the input value is a number and it is positive
+        public static bool IsPositiveInt(String input)
+        {
+            if (Int32.TryParse(input, out int intResult) && intResult > 0) return true;
+            else return false;
+        }
+
+        // Checks if the input value is a number and it is both positive and odd
+        public static bool IsPositiveOddInt(String input)
+        {
+            if (IsPositiveInt(input))
+            {
+                if (Int32.Parse(input) % 2 == 0) return false;
+                else return true;
+            }
+            else return false;
+        }
+    }
+}
---
+++ Task0/Functions.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using static System.Math;
+
+namespace com.GitHub.Reiqen
+{
+    /*
+        This class provides methods to perform actions for task needs.
+        It has an interface to create a sequence, show simplicity information, print a square of '*' symbols,
+        create a jagged array and sort it by subarray values and all values.
+    */
+    class Functions
+    {
+        // Creates a sequence of a range from 1 to the number from received argument
+        public static void MakeSequence(int arg)
+        {
+            for (var i = 1; i <= arg; i++) Console.Write(i + " ");
+        }
+
+        // Shows if the number from received argument is simple
+        public static void SimplicityInfo(int arg)
+        {
+            var checker = true;
+                for (var i = 2; i<=Sqrt(arg); i++)
+                {
+                    if (arg % i == 0)
+                    {
+                        checker = false;
+                        break;
+                    }
+                    else checker = true;
+                }
+            if (checker) Console.WriteLine("Число является простым");
+            else Console.WriteLine("Число не является простым");
+        }
+
+        // Prints a square of '*' symbols with a hole in the middle
+        public static void PrintSquare(int arg)
+        {
+            for (var i = 0; i < arg; i++)
+            {
+                for (var j = 0; j < arg; j++)
+                {
+                    if (i == arg / 2 && j == arg / 2) Console.Write("  ");
+                    else Console.Write("* ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        // Prints a jagged array filled with random numbers and returns it.
+        // Takes a parameter of array of values where size of each subarray is defined.
+        // Further return is made for sending it to the sort method.
+        public static int[][] PrintAndReturnUnsortedArray(int[] arrayOfValues)
+        {
+            var randomNumber = new Random();
+            var unsortedArray = new int[arrayOfValues.Length][];
+            Console.WriteLine("\nНесортированный массив:\n");
+            for (var i = 0; i < arrayOfValues.Length; i++)
+            {
+                unsortedArray[i] = new int[arrayOfValues[i]];
+                Console.Write("Подмассив " + (i + 1) + ": [");
+                for (var j = 0; j < arrayOfValues[i]; j++)
+                {
+                    unsortedArray[i][j] = randomNumber.Next(0, 100);
+                    if (j + 1 == arrayOfValues[i]) Console.Write(unsortedArray[i][j]);
+                    else Console.Write(unsortedArray[i][j] + " ");
+                }
+                Console.WriteLine("]");
+            }
+            return unsortedArray;
+        }
+
+        // Provides sorting by subarray values and all values
+        public static void PrintSortedArray(int[][] unsortedArray)
+        {
+            // Sort by subarray values
+            Console.WriteLine("\nОтсортированный по значениям в подмассиве массив:\n");
+            var sortedArray = unsortedArray;
+            for (var i = 0; i < sortedArray.Length; i++)
+            {
+                Array.Sort(sortedArray[i]);
+                Console.Write("Подмассив " + (i + 1) + ": [");
+                for (var j = 0; j < sortedArray[i].Length; j++)
+                {
+                    if (j+1 == sortedArray[i].Length) Console.Write(unsortedArray[i][j]);
+                    else Console.Write(unsortedArray[i][j] + " ");
+                }
+                Console.WriteLine("]");
+            }
+
+            // Sort by all values
+            Console.WriteLine("\nОтсортированный по всем значениям массив:\n");
+            int counter = 0;
+            for (var i = 0; i < sortedArray.Length; i++)
+            {
+                for (var j = 0; j < sortedArray[i].Length; j++) counter++;
+            }
+            var tempArray = new int[counter];
+            counter = 0;
+            for (var i = 0; i < sortedArray.Length; i++)
+            {
+                for (var j = 0; j < sortedArray[i].Length; j++)
+                {
+                    tempArray[counter] = sortedArray[i][j];
+                    counter++;
+                }
+            }
+            counter = 0;
+            Array.Sort(tempArray);
+            for (var i = 0; i < sortedArray.Length; i++)
+            {
+                Console.Write("Подмассив " + (i + 1) + ": [");
+                for (var j = 0; j < sortedArray[i].Length; j++)
+                {
+                    sortedArray[i][j] = tempArray[counter];
+                    counter++;
+                    if (j + 1 == sortedArray[i].Length) Console.Write(unsortedArray[i][j]);
+                    else Console.Write(unsortedArray[i][j] + " ");
+                }
+                Console.WriteLine("]");
+            }
+        }
+    }
+}
