3a0f6a22c928ec7a3c61406cbf3927acae3fecc2
Roman Kuznetcov <reiqen@yahoo.com>
2019-12-15 15:17:44 +0400

    Add task4

--- Task4/CustomSort.cs
+++ Task4/CustomSort.cs
@@ -2,31 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace com.GitHub.Reiqen.Task4
 {
     class CustomSort
     {
+        public delegate void SortingHandler(string message);
+        public event SortingHandler Notify;
 
-        public static void Sort<T>(T[] arr, Func<T, T, int> compare)
+
+        public void Sort<T>(T[] arr, Func<T, T, int> compare)
         {
             if (compare == null)
             {
                 throw new ArgumentException();
             }
-
+            T temp;
             for (int i = 0; i < arr.Length; i++)
             {
                 for (int j = (i + 1); j < arr.Length; j++)
                 {
                     if (compare(arr[j], arr[i]) > 0)
                     {
-                        var temp = arr[i];
+                        temp = arr[i];
                         arr[i] = arr[j];
                         arr[j] = temp;
                     }
                 }
+            }
+            Notify?.Invoke("Sorting Finished");
+        }
+
+        public void ThreadSort<T>(T[] arr, Func<T, T, int> compare)
+        {
+            lock ("")
+            {
+                Task.Factory.StartNew(() => Sort(arr, compare));
             }
         }
 
@@ -39,7 +52,7 @@ namespace com.GitHub.Reiqen.Task4
             if (s.Length < t.Length) return 1;
             if (s.Length > t.Length) return -1;
 
-            return s.CompareTo(t);
+            return t.CompareTo(s);
         }
 
         public static int CompareDouble(double s, double t)
@@ -49,7 +62,37 @@ namespace com.GitHub.Reiqen.Task4
             if (s < t) return 1;
             if (s > t) return -1;
 
-            return s.CompareTo(t);
+            return t.CompareTo(s);
+        }
+
+        public static int Sum(int[] arr)
+        {
+            int sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                sum += arr[i];
+            }
+            return sum;
+        }
+
+        public void ShowSum(int[] arr, Func <int[], int> operation)
+        {
+            Console.WriteLine("Результат операции: " + operation(arr));
+        }
+
+        public static bool Check(string str)
+        {
+            char[] charArr = str.ToCharArray();
+            foreach (char ch in charArr)
+            {
+                if (!Char.IsDigit(ch)) return false;  
+            }
+            return true;
+        }
+
+        public void ShowString(string str, Func<string, bool> checker)
+        {
+            Console.WriteLine("Результат операции: " + checker(str));
         }
     }
 }
--- Task4/Program.cs
+++ Task4/Program.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace com.GitHub.Reiqen.Task4
 {
@@ -11,25 +12,40 @@ namespace com.GitHub.Reiqen.Task4
     {
         static void Main(string[] args)
         {
-                string[] array = new string[] { "bacd", "abcd", "ff", "rty", "r" };
-                Func<string, string, int> cs = CompareString;
-                Sort(array, cs);
-
-                for (int i = 0; i < array.Length; i++)
-                {
-                    Console.WriteLine(array[i]);
-                }
+            CustomSort customSort = new CustomSort();
+            customSort.Notify += SortingFinished;
+            string[] array = new string[] { "bacd", "abcd", "ff", "rty", "r", "ss", "uu", "aa" };
+            Func<string, string, int> cs = CompareString;
+            customSort.Sort(array, cs);
+            // or customSort.ThreadSort(array, cs);
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
 
             double[] arrayDouble = new double[] { 2F, 5F, 6F, 1F, -1F };
             Func<double, double, int> csDouble = CompareDouble;
-            Sort(arrayDouble, csDouble);
-
+            customSort.Sort(arrayDouble, csDouble);
+            // or customSort.ThreadSort(arrayDouble, csDouble);
             for (int i = 0; i < arrayDouble.Length; i++)
             {
                 Console.WriteLine(arrayDouble[i]);
             }
 
+            int[] arrayInt = new int[] { 2, 5, 6, 1, -1 };
+            Func<int[], int> csInt = Sum;
+            customSort.ShowSum(arrayInt, csInt);
+
+            string str = "-10";
+            Func<string, bool> csString = Check;
+            customSort.ShowString(str, csString);
+
+
             Console.ReadLine();
+        }
+        public static void SortingFinished(String message)
+        {
+            Console.WriteLine(message);
         }
     }
 }
